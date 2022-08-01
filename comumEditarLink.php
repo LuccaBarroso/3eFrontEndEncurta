696471a8915c719b11f1f9de18e5cf3c07cba919
Lucca <barrosolucca@gmail.com>
2022-08-01 08:56:11 -0300

    Criar link usuário

---
+++ comumEditarLink.php
@@ -0,0 +1,52 @@
+<?php
+  include("top.php");
+  include("navBar.php");
+?>
+
+<div class="container-fluid content">
+
+  <!-- BreadCrumb -->
+  <div class="breadCrumbHolder my-5">
+    <div class="breadCrumb1 breadCrumb d-inline">
+        <img src="/imgs/breadCrumb1.svg" class="img-responsive">
+        <a href="#" class="overlay-text centeredText">Deslogado</a>
+    </div>
+    <div class="breadCrumb1 breadCrumb d-inline">
+        <img src="/imgs/breadCrumb3.svg" class="img-responsive" style="margin-left: -10px;">
+        <a href="#" class="overlay-text centeredText">Editar Link</a>
+    </div>
+  </div>
+
+  <!-- Content -->
+  <div class="card shadow p-3 m-1 pt-4 bg-body rounded-lg border-0 d-inline">
+    
+    <div class="d-flex flex-column">
+      <h4 class="text-center w-100">Editar Link</h4>
+       <form class="m-auto">
+
+        <label for="link">Link:</label>
+        <input type="text" class="textInput my-2" name="link" placeholder="https://site.3esolucoes.com.br/" />
+        
+        <label for="link">Selecione um Projeto</label>
+        <select class="textInput form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Abrir Menu</option>
+            <option value="1">Projeto A</option>
+            <option value="2">Projeto B</option>
+            <option value="3">Projeto C</option>
+        </select>
+        
+        <div class="d-flex justify-content-center">
+            <button type="submit" class="btn-primary px-4 mt-5 mb-2 growHover">Atualizar</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
+<?php
+  include("bottom.php");
+?>
+
+    
+
+
